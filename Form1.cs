f1b75de6f494bacf86277fd870515ccfec783b82
Kulakov D.A., ISTb-21-1 <dragonipam@yandex.ru>
2023-01-06 10:45:13 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace ParticleSystem_KulakovDA_ISTb_21_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
